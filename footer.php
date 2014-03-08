171c1624bdb1aa92d6ba6270f0315a591ee009ca
jimogios <dimitris.moraitidis@gmail.com>
2014-03-08 15:30:05 +0200

    try to fix footer stupid problem

--- footer.php
+++ footer.php
@@ -1,13 +1,15 @@
+		<!--
 		<div id="footer">
 			&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?>
-		</div>
+		</div>-->
+		
 
 	</div>
 
 	<?php wp_footer(); ?>
 	
 	<!-- Don't forget analytics -->
-	
+
 </body>
 
 </html>

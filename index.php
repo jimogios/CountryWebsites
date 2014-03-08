86baa680a2e7cd2fcbac155225976e5946eaaca7
q2620533 <simonpanzihao@gmail.com>
2014-03-08 21:32:05 +0800

    youth-1 input to index.php

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 <<<<<<< HEAD
 <div class="row-fluid text-center">
 	
-	<div class="span6"><img src="/wordpress/wp-content/themes/CountryWebsites/img/Corporate-1.png"</div>
+	<div class="span6"><img src="/wordpress/wp-content/themes/CountryWebsites/img/Corporate-1.png"></div>
 	
-	<div class="span6">test</div>
+	<div class="span6"><img src="/wordpress/wp-content/themes/CountryWebsites/img/youth-1.jpg"></div>
 	
 	
 	

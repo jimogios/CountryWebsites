fca640ef55c6056c223c8e0f0878ae2de72b5df1
jimogios <dimitris.moraitidis@gmail.com>
2014-03-08 13:19:44 +0200

    edited the youthindex a little bit

---
+++ youthindex.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<div class="row fluid">
+
+<div class="span4 text-center"> xxxx1</div>
+<div class="span4 text-center"> xxxx2</div>
+<div class="span4 text-center"> xxxx3</div>
+
+</div>
+<?php get_footer(); ?>

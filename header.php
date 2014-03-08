1a2fa54a8ac1ebc461d3d74ce295bddb1e931a36
jimogios <dimitris.moraitidis@gmail.com>
2014-03-08 11:48:23 +0200

    2nd commit

--- header.php
+++ header.php
@@ -1,55 +1,161 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>PARALLAXER | responsive parallax Bootstrap template</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="responsive parallax Bootstrap template">
+<meta name="author" content="ArtLabs">
 
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<!-- ######################## Styles ######################
+	=======================================================-->
+	<!--<link href="<?php bloginfo("template_url");?>animate.min.css" rel="stylesheet">-->
+<link href="<?php bloginfo("template_url");?>/css/bootstrap.css" rel="stylesheet">
+<link href="<?php bloginfo("template_url");?>/css/bootstrap-responsive.css" rel="stylesheet">
 
-<head profile="http://gmpg.org/xfn/11">
-	
-	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-	
-	<?php if (is_search()) { ?>
-	   <meta name="robots" content="noindex, nofollow" /> 
-	<?php } ?>
 
-	<title>
-		   <?php
-		      if (function_exists('is_tag') && is_tag()) {
-		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
-		      elseif (is_archive()) {
-		         wp_title(''); echo ' Archive - '; }
-		      elseif (is_search()) {
-		         echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
-		      elseif (!(is_404()) && (is_single()) || (is_page())) {
-		         wp_title(''); echo ' - '; }
-		      elseif (is_404()) {
-		         echo 'Not Found - '; }
-		      if (is_home()) {
-		         bloginfo('name'); echo ' - '; bloginfo('description'); }
-		      else {
-		          bloginfo('name'); }
-		      if ($paged>1) {
-		         echo ' - page '. $paged; }
-		   ?>
-	</title>
-	
-	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-	
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
-	
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+<link href="<?php bloginfo("template_url");?>/css/animate.min.css" rel="stylesheet">
+<link href="<?php bloginfo("template_url");?>/css/style.css" rel="stylesheet">
+<noscript>
+<link href="<?php bloginfo("template_url");?>/css/noscript.css" rel="stylesheet">
+</noscript>
 
-	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 
-	<?php wp_head(); ?>
+
+<!-- FontAwesome styles (iconic font)-->
+<link href="<?php bloginfo("template_url");?>/css/font-awesome.min.css" rel="stylesheet"> 
+<!-- prettyPhoto styles -->
+
+<!-- Gallery inlarge script styles-->
+<link href="<?php bloginfo("template_url");?>/css/prettyPhoto.css" rel="stylesheet" type="text/css" />
+
+<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
 	
+<!-- Google web fonts -->
+<link href='http://fonts.googleapis.com/css?family=Sintony:400,700&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,300italic,500,400italic,500italic,700,700italic,900italic,900&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
+<link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic,700italic&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
+<!-- Le fav and touch icons -->
+<link rel="shortcut icon" href="assets/ico/favicon.ico">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
 </head>
 
-<body <?php body_class(); ?>>
+<body data-spy="scroll" data-target=".scroller-spy" data-twttr-rendered="true">
+
+<!--################# START MAIN-WRAPPER ####################
+	============================================================ --> 
+<div class="main-wrapper">
+
+	<!-- ######################  START HEADER ######################
+		==============================================================-->
+	<header class="headertop needhead" id="header-section">
+		
+		<!-- START PARALLAX BG -->
+		<div class="innerbg">
 	
-	<div id="page-wrap">
+<!-- ###################### START TOP MENU ####################
+============================================================== -->
+<div class="nav-top fixed action" id="top-nav"> 
+	<!-- navbar-fixed-top -->
+    <div class="navbar" > 
+	<!-- INNER-->
+      <div class="navbar-inner">
+        <div class="container"> 
+		
+		<!-- Collapse button -->	
+			<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+						<!-- LOGO -->
+						<a class="brand" href="index.html"> 
+						<span><i class="fa-icon-cogs"></i> PARALLAXER</span>
+						<!--<img src="assets/img/logo.png" alt="Logo">--></a>
+						
+			 <!-- TOP NAVIGATION-->
+			 <div id="main-nav" class="scroller-spy">
+				<nav class="nav-collapse collapse" >
+				  <ul class="nav pull-right" id="nav">
+					<li class="active"><a href="#header-section">Home</a> </li>
+					<li><a href="#features-section">Service</a> </li>	
+					<li><a href="#portfolio-section">Works</a> </li>
+					<li><a href="#slider-section">Sliders</a> </li>
+					<li><a href="#team-section">Team</a> </li>
+					<li><a href="#contact-section">Contact</a> </li>
+				  </ul>
+				</nav>
+			  </div>
+		  
+        </div>
+      </div>
+    </div>
+</div>
+<!-- ############################ END TOP MENU ###############
+	========================================================== -->		
 
-		<div id="header">
-			<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
-			<div class="description"><?php bloginfo('description'); ?></div>
-		</div>
+<!-- HEADER MAIN-->	
 
+	<!-- ============ SLIDER ============= -->
+
+			<div id="carousel_horizontal_slide_2" class="carousel slide slider_top">
+				<div class="carousel-inner">
+				
+					<!--########## item #############-->
+					<div class="item active">
+						<div class="hero-unit text-center">
+						<h1 class="">
+						
+						<span class="active">PARALLAXER	</span></h1>
+						<!-- !! DEL Class="active" when used static animated h1 script -->
+								<h2 class="text-center">
+									Start &amp; grow faster with PARALLAXER
+								</h2>
+							</div>
+								
+						
+					</div>
+					<!--############/item###########-->
+					
+					<!--########## item #############-->
+					<div class="item">
+					<img src="<?php bloginfo("template_url");?>/img/browserthree.png" alt="" />
+					</div>
+					<!--############/item###########-->
+					
+					<!--########## item #############-->
+					<div class="item">
+					<img src="<?php bloginfo("template_url");?>/img/responsiveshwcase.png" alt="" />
+					</div>
+					<!--############/item###########-->
+					
+					
+					
+				</div>
+				
+				<a class="carousel-control left btn-control " href="#carousel_horizontal_slide_2" data-slide="prev"><i class="fa-icon-double-angle-left main-color"></i></a>
+				<a class="carousel-control right btn-control btn-action" href="#carousel_horizontal_slide_2" data-slide="next"><i class="fa-icon-double-angle-right main-color"></i></a>
+	</div>
+	
+
+
+</div>
+
+<div class="action-wrapper" id="startbtn">
+	<a class="btn btn-large btn-primary btn-action-bottom" href="#features-section"  title="">
+	START NOW
+	</a>
+</div>
+<div class="leftbg"></div>
+<!--/ END PARALLAX BG-->
+</header>
+<!-- / ###################### END MAIN HEADER ####################
+	=============================================================== -->

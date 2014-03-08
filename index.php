499c11684060aa43ad6b6a9e2ff5fe477eb81037
jericadriano <jeric.adriano@aiesec.net>
2014-03-08 21:21:15 +0800

    Corporate 1 Image input into Index Php

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php get_header(); ?>
 
 
+<div class="row-fluid text-center">
+	
+	<div class="span6"><img src="/wordpress/wp-content/themes/CountryWebsites/img/Corporate-1.png"</div>
+	
+	<div class="span6">test</div>
+	
+	
+	
+</div>
+
+
+
+
 <?php get_footer(); ?>

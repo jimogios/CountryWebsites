de8b7b6c03822ce3141b3ea5d7bf82815eda9c10
jimogios <dimitris.moraitidis@gmail.com>
2014-03-08 15:55:36 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,10 +1,257 @@
-		<!--
-		<div id="footer">
-			&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?>
-		</div>-->
-		
 
+<!-- ###################### FOOTER #######################
+    ================================================== -->
+<footer class="footer" id="contact-section">
+  
+   
+<!-- CONTACT HEADER-->
+<div class="">
+<div class="foothead">
+	<div class="container">
+		<div class="row-fluid">
+			<div class="span12">
+				<div class="page-header">
+					<h3><small>Stay in touch</small>CONTACT US </h3>
+				</div>
+			</div>
+		</div>
 	</div>
+</div>	
+</div>
+<!--/ CONTAC HEADER-->
+
+
+
+ <div class="container">
+<div class="row-fluid">
+
+<div class="span6 comments-form-line">
+   
+        <form accept-charset="UTF-8" method="POST" class="comments-form row-fluid">
+
+                    <p>
+                      <label for="name"><strong>Name</strong> <span class="main-color">*</span></label>
+                      <input class="span12" type="text" id="name" name="name" placeholder="Name" value="">
+                    </p>
+
+                     
+					<p>
+                      <label for="email"><strong>Email</strong> <span class="main-color">*</span></label>
+                      <input class="span12" id="email" type="text" name="email" placeholder="Email" value="" required="">
+                    </p>
+
+                    
+
+					<p>
+					<label for="message"><strong>Message</strong></label>
+					<textarea class="span12" id="message" name="message" placeholder="Type in your comment" rows="5"></textarea>
+					</p>
+					 
+					 <p>
+                    <button class="btn btn-action medium" type="submit">SEND MESSAGE</button>
+					 </p>
+
+					 </form>
+</div>
+	
+<div class="span3">
+    <h4 class="line3 center standart-h4title"><span>Useful Links</span></h4>
+        <ul class="footer-links">
+          <li><a href="#">PARALLAXER</a></li>
+          <li><a href="#">PARALLAXER</a></li>
+          <li><a href="#">WordPress.com</a></li>
+          <li><a href="#">ThemeForest.net</a></li>
+          <li><a href="#">Free Vector Icons</a></li>
+        </ul>
+</div><div class="span3">
+    <h4 class="line3 center standart-h4title"><span>Useful Links</span></h4>
+        <ul class="footer-links">
+          <li><a href="#">PARALLAXER</a></li>
+          <li><a href="#">PARALLAXER</a></li>
+          <li><a href="#">WordPress.com</a></li>
+          <li><a href="#">ThemeForest.net</a></li>
+          <li><a href="#">Free Vector Icons</a></li>
+        </ul>
+</div>
+
+<div class="span6">
+<hr class="line" />
+</div>
+
+<div class="span3">
+
+<h4> Lets connected</h4>
+		<ul class="social-icons">
+			<li><a href="#" title=""><i class="fa-icon-facebook"></i></a></li>
+			<li><a href="#" title=""><i class="fa-icon-twitter"></i></a></li>
+			<li><a href="#" title=""><i class="fa-icon-github-alt"></i></a></li>
+			<li><a href="#" title=""><i class="fa-icon-google-plus"></i></a></li>
+			<li><a href="#" title=""><i class="fa-icon-linkedin"></i></a></li>
+		</ul>
+   
+		
+		
+</div>
+
+<div class="span3">
+
+<h4> Our office</h4>
+		<address>
+				<strong class="main-color">PARALLAXER, LLC.</strong><br>
+				<i class="fa-icon-map-marker"></i> 795 Folsom Ave, Suite 600
+				San Francisco, CA 94107<br/>
+				<i class="fa-icon-phone-sign"></i> + 4 (123) 456-7890
+				<div class="foot-line"></div>
+		</address>
+	
+</div>
+
+</div>
+<!-- ROW-FLUID-->
+</div>
+
+<div class="map-wrapper">
+			<!--<div class="adrinner">
+				<address>
+				<strong class="main-color">PARALLAXER, Inc.</strong><br>
+				<i class="fa-icon-map-marker"></i> 795 Folsom Ave, Suite 600
+				San Francisco, CA 94107<br/>
+				<i class="fa-icon-phone-sign"></i> + 4 (123) 456-7890
+				<div class="foot-line"></div>
+				</address>
+			</div>-->
+			<!-- MAP DIV // !Don't remove this !Important -->
+			<div id="map"></div>
+			<!-- MAP DIV // !Don't remove this !Important -->
+		</div>
+
+</footer>
+<!-- END: FOOTER -->
+
+
+</div>
+<!-- END: MAIN-WRAPPER-->
+
+
+<!-- Le javascript
+    ================================================== --> 
+<!-- Placed at the end of the document so the pages load faster --> 
+
+<script src="<?php bloginfo("template_url");?>/js/jquery.js" type="text/javascript"></script> 
+<script src="<?php bloginfo("template_url");?>/js/bootstrap.min.js" type="text/javascript"></script> 
+<script src="<?php bloginfo("template_url");?>/js/jquery.easing.js" type="text/javascript" ></script>
+<script src="<?php bloginfo("template_url");?>/js/jquery.prettyPhoto-3.1.4-W3C.js" type="text/javascript"></script> 
+<script src="<?php bloginfo("template_url");?>/js/jquery.ui.totop.js" type="text/javascript"></script>
+<script type="text/javascript" src="<?php bloginfo("template_url");?>/js/jquery.inview.js"></script>
+<script src="<?php bloginfo("template_url");?>/js/jquery.parallax-1.1.3.js" type="text/javascript" ></script>
+<script src="<?php bloginfo("template_url");?>/js/jquery.localscroll-1.2.7-min.js" type="text/javascript" ></script>
+<script src="<?php bloginfo("template_url");?>/js/jquery.scrollTo-1.4.2-min.js" type="text/javascript" ></script>
+<script src="<?php bloginfo("template_url");?>/js/jquery.inview.js" type="text/javascript" ></script>
+
+<!-- VIDEO -->
+<script src="<?php bloginfo("template_url");?>/js/jquery.fitvids.min.js" type="text/javascript"></script>
+
+
+<!-- gMap PLUGIN -->
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript" src="<?php bloginfo("template_url");?>/js/jquery.gmap.js"></script>
+<!-- gMap PLUGIN -->
+
+<!-- PAGE CUSTOM SCROLLER -->
+<script type="text/javascript" src="<?php bloginfo("template_url");?>/js/jquery.nicescroll.min.js"></script>
+
+<!-- INIT SCRIPTS -->
+<script type="text/javascript" src="<?php bloginfo("template_url");?>/js/custom.js"></script>
+		
+<script>
+        // Basic FitVids Test
+        jQuery(".container").fitVids();
+          
+</script>
+		
+		
+<script>
+	jQuery('.carousel').carousel({
+		interval: 7000
+	})
+</script>
+
+	
+<script>
+	jQuery(document).ready(
+  function() {  
+    jQuery("html").niceScroll({cursorcolor:"#282828"});
+  }
+);
+</script>
+
+<script>
+//make section height of window
+	jQuery(function(){
+		jQuery('#header-section').css({'height':(jQuery(window).height())+'px'});
+		jQuery(window).resize(function(){
+		jQuery('#header-section').css({'height':(jQuery(window).height())+'px'});
+		});
+	});
+</script>
+
+<script>
+jQuery('.thumbnail').hover(function(){
+				jQuery(this).find('.img-effect').stop().animate({'opacity':'0.7'});
+				jQuery(this).find('.img-links').stop().animate({'top':'50%'});
+				 },function(){
+				 jQuery(this).find('.img-effect').stop().animate({'opacity':'0'});
+				jQuery(this).find('.img-links').stop().animate({'top':'-50%'});
+			});
+</script>
+
+<script type="text/javascript">
+		
+				jQuery(document).ready(function(){
+
+				jQuery('#map').gMap({ address: 'Folsom Ave, San Francisco, CA',
+							   panControl: true,
+						   zoomControl: true,
+							   zoomControlOptions: {
+					style: google.maps.ZoomControlStyle.SMALL
+							   },
+						   mapTypeControl: true,
+						   scaleControl: true,
+						   streetViewControl: false,
+						   overviewMapControl: true,
+							   scrollwheel: true,
+							   icon: {
+						image: "http://www.google.com/mapfiles/marker.png",
+						shadow: "http://www.google.com/mapfiles/shadow50.png",
+						iconsize: [20, 34],
+						shadowsize: [37, 34],
+						iconanchor: [9, 34],
+						shadowanchor: [19, 34]
+					},
+						zoom:15,
+							   markers: [
+							{ 'address' : 'Folsom Ave, San Francisco, CA'}
+						]
+							   
+							   
+							   }); 
+				});
+
+
+
+</script>
+<script type="text/javascript">
+		
+	jQuery('#my-tab a').click(function (e) {
+  e.preventDefault();
+  jQuery(this).tab('show');
+})
+</script>
+
+
+
+</body>
+</html>
 
 	<?php wp_footer(); ?>
 	
